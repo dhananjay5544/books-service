2c5bcb5add585013bbd071596702274a714fbc7d
Dhananjay Shinde <Dhananjay.Shinde@emtecinc.com>
2021-02-10 20:34:17 +0530

    typeorm,consumer setup and dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      host: 'localhost',
+      port: 3306,
+      username: 'testuser',
+      password: 'root123',
+      database: 'library',
+      type: 'mysql',
+      synchronize: true,
+      logging: false,
+      entities: ['./dist/**/**.entity.js'],
+    }),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
