d0b35f0998d1c6cba15fb0290ab03477a5d95cc0
Dhananjay Shinde <Dhananjay.Shinde@emtecinc.com>
2021-02-10 20:57:33 +0530

    book-resolver removed

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,13 +1,12 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { BookController } from './book.controller';
-import { BookResolver } from './book.resolver';
 import { BookService } from './book.service';
 import { Book } from './book.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book])],
   controllers: [BookController],
-  providers: [BookResolver, BookService],
+  providers: [BookService],
 })
 export class BookModule {}
