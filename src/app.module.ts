c21e7206aa59703289446292d610da2f925c190c
Dhananjay Shinde <Dhananjay.Shinde@emtecinc.com>
2021-02-10 21:09:20 +0530

    env update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,14 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { TYPEORM } from './environments';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    }),
     TypeOrmModule.forRoot({
-      host: 'localhost',
-      port: 3306,
-      username: 'testuser',
-      password: 'root123',
-      database: 'library',
+      ...TYPEORM,
       type: 'mysql',
       synchronize: true,
       logging: false,
